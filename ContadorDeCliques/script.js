abdf7719f142047cac809f86915fa3fd8f30d762
José Júnior <59178745+josec-junior@users.noreply.github.com>
2024-02-28 23:09:57 -0300

    Implementação da funcionalidade de modo noturno/escuro

--- ContadorDeCliques/script.js
+++ ContadorDeCliques/script.js
@@ -1,7 +1,39 @@
-var contador = 0
-var i = 30
+let contador = 0
+let i = 30
 cliques = document.getElementById("cliques")
 tempo = document.getElementById("tempo")
+
+function mudarTema() {
+    let corpo = window.document.getElementsByTagName("body")[0]
+    let titulo = window.document.getElementsByTagName("h1")[0]
+    let divCliques = window.document.getElementById("cliques")
+    let divTempo = window.document.getElementById("tempo")
+    let button = window.document.getElementsByClassName("btn")[0]
+    let rodape = window.document.getElementsByTagName("footer")[0]
+    let link1 = rodape.getElementsByClassName("link")[0]
+    let link2 = rodape.getElementsByClassName("link")[1]
+    if (corpo.style.backgroundColor == "white" || corpo.style.backgroundColor == "") {
+        corpo.style.backgroundColor = "rgb(44, 43, 43)"
+        corpo.style.fontStyle = "white"
+        titulo.style.color = "#be1dd6"
+        divCliques.style.color = "white"
+        divTempo.style.color = "white"
+        button.style.backgroundColor = "#be1dd6"
+        rodape.style.color = "white"
+        link1.style.color = "#be1dd6"
+        link2.style.color = "#be1dd6"
+    } else {
+        corpo.style.backgroundColor = "white"
+        corpo.style.fontStyle = "black"
+        titulo.style.color = "#0b0be0"
+        divCliques.style.color = "black"
+        divTempo.style.color = "black"
+        button.style.backgroundColor = "#0b0be0"
+        rodape.style.color = "black"
+        link1.style.color = "#0b0be0"
+        link2.style.color = "#0b0be0"
+    }
+}
 
 function clicar() {
     if(contador == 0) {
