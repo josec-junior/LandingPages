6e4d47598383a1449df5a9f045b002d1f851119b
José Júnior <59178745+josec-junior@users.noreply.github.com>
2024-07-13 13:46:03 -0300

    Adição de imagem do São João da cidade e modal para as imagens

--- MeuBairro/scripts/script.js
+++ MeuBairro/scripts/script.js
@@ -5,3 +5,32 @@ function mostrarAnoAtual() {
     let anoAtual = dataAtual.getFullYear()
     paragrafo.innerHTML += ` &copy; ${anoAtual}`
 }
+
+function abrirModalParaImagens(classeDasImagens) {
+  // Get the modal
+  let modal = document.getElementsByClassName("modal")[0]
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  let imagemDoModal = document.getElementById("modalImg")
+  let legendaDoModal = document.getElementById("legenda")
+
+  // Get all images
+  let imgs = document.getElementsByClassName(classeDasImagens)
+
+  // Add onclick event to each image
+  for (let img of imgs) {
+    img.onclick = function() {
+      modal.style.display = "block"
+      imagemDoModal.src = this.src
+      legendaDoModal.innerHTML = this.alt
+    }
+  }
+
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("fecharModal")[0]
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+}
